b2c24b72e9cbc707e020b2326043c13bc876890e
namnh06 <namnh.developer@gmail.com>
2019-10-18 21:21:57 +0700

    finish php4

---
+++ php/04-20191018/5-select-data.php
@@ -0,0 +1,30 @@
+<?php
+$serverName = "localhost";
+$userName = "root";
+$password = "";
+$dbName = "ap_21_4";
+
+// Create connection
+$conn = new mysqli($serverName, $userName, $password, $dbName);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT * FROM ap_21_4.users";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+  // output data of each row
+  while ($row = mysqli_fetch_assoc($result)) {
+    if ($row["email"] == "nam@gmail.com") {
+      echo "Dang Nhap Thanh Cong";
+    } else {
+      echo "<h1>Chuc mung 20/10</h1>";
+    }
+  }
+} else {
+  echo "0 results";
+}
+
+mysqli_close($conn);
---
+++ php/04-20191018/7-check.php
@@ -0,0 +1,8 @@
+<?php
+
+?>
+
+<h1>Xin chao <?php echo $_POST["nam"]; ?></h1>
+<!-- if($row["email"] == $_POST["nam"]){
+
+} -->
