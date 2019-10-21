4ce89cb29789ff20aff762510bd4083001d6c50d
namnh06 <namnh.developer@gmail.com>
2019-10-21 21:21:03 +0700

    finish php5

--- php/05-20191021/role_user.sql
+++ php/05-20191021/role_user.sql
@@ -18,3 +18,23 @@ CREATE TABLE ap_21_5.users (
 );
 
 SELECT * FROM ap_21_5.users;
+
+INSERT INTO ap_21_5.users 
+(email, password, role_id)
+VALUES
+('a@gmail.com','123456',1);
+
+SELECT * FROM ap_21_5.users AS a
+JOIN ap_21_5.roles as b
+ON a.role_id = b.id;
+
+
+INSERT INTO ap_21_5.users 
+(email, password, role_id)
+VALUES
+('b@gmail.com','123456',3),
+('c@gmail.com','123',2);
+
+SELECT * FROM ap_21_5.users AS a
+JOIN ap_21_5.roles as b
+ON a.role_id = b.id;
