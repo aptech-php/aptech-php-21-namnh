fb580f1201190a6c7797cac11f2ae565d81fb858
namnh06 <namnh.developer@gmail.com>
2019-11-04 21:21:17 +0700

    finish php10

---
+++ php/10-20191104/ap-21-10/resources/views/users.blade.php
@@ -0,0 +1,18 @@
+<table>
+  <thead>
+    <tr>
+      <th>id</th>
+      <th>name</th>
+      <th>phone number</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach($users as $user)
+    <tr>
+      <td>{{$user->id}}</td>
+      <td>{{$user->name}}</td>
+      <td>{{$user->phone->phone_number}}</td>
+    </tr>
+    @endforeach
+  </tbody>
+</table>
